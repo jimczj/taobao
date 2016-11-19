31cdb01f249ff560060bc38f61b1c4c4225904e4
jimczj <jimczj@gmail.com>
2016-11-18 18:39:18 -0800

    initUser

---
+++ models/utils/initUser.js
@@ -0,0 +1,53 @@
+var Role = require('../Role');
+var User = require('../User');
+var mongoose = require('mongoose');
+var config = require('../../config');
+
+mongoose.connect(config.mongodb);
+
+Role.findOne({
+  role_name: 'root'
+}, function(err, role) {
+  if (err) {
+    console.log(err)
+  } else {
+    let user = User({
+      username: 'root',
+      password: 'root',
+      role_id: role._id,
+    });
+    user.save(function(err) {
+      if (err) {
+        console.log("create root user fail")
+      } else {
+        console.log("create root user success")
+      }
+    });
+  }
+});
+
+
+Role.findOne({
+  role_name: 'customer'
+}, function(err, role) {
+  if (err) {
+    console.log(err)
+  } else {
+    let user = User({
+      username: 'customer',
+      password: 'customer',
+      role_id: role._id,
+    });
+    user.save(function(err) {
+      if (err) {
+        console.log("create customer user fail")
+      } else {
+        console.log("create customer user success")
+      }
+    });
+  }
+});
+
+mongoose.disconnect(()=>{
+  console.log('完成初始化');
+})
